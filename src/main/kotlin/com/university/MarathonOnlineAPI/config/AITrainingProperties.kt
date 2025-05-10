7585567913d22e4d7dd02e964b02fd3a76543e91
Nguyễn Hà Quỳnh Giao <21110171@student.hcmute.edu.vn>
2025-05-10 20:41:14 +0700

    Fix Config App for AI - training plan v2

--- src/main/kotlin/com/university/MarathonOnlineAPI/config/AITrainingProperties.kt
+++ src/main/kotlin/com/university/MarathonOnlineAPI/config/AITrainingProperties.kt
@@ -1,9 +1,7 @@
 package com.university.MarathonOnlineAPI.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.stereotype.Component
 
-@Component
 @ConfigurationProperties(prefix = "ai-training")
 data class AITrainingProperties(
     var api: String = ""
