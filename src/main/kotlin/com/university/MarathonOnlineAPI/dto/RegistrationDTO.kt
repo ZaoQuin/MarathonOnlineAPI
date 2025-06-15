95c6b0230c59e6ee2e647c30c27069384770a911
Nguyễn Hà Quỳnh Giao <21110171@student.hcmute.edu.vn>
2025-06-15 22:30:40 +0700

    Fix records prop of registrations

--- src/main/kotlin/com/university/MarathonOnlineAPI/dto/RegistrationDTO.kt
+++ src/main/kotlin/com/university/MarathonOnlineAPI/dto/RegistrationDTO.kt
@@ -10,7 +10,7 @@ data class RegistrationDTO (
     var registrationDate: LocalDateTime? = null,
     var completedDate: LocalDateTime? = null,
     var registrationRank: Int? = null,
-    var races: List<RecordDTO>? = null,
+    var records: List<RecordDTO>? = null,
     var rewards: List<RewardDTO>? = null,
     var status: ERegistrationStatus? = null
 )
